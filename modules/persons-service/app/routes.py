de138dec862817f5dba639ae0feb87be13ff80e3
li1515 <ihnatenkolv@gmail.com>
2024-01-21 00:31:50 +0100

    Added services

--- modules/persons-service/app/routes.py
+++ modules/persons-service/app/routes.py
@@ -1,5 +1,5 @@
-def register_routes(api, app, root="api"):
+def register_routes(persons, app, root="persons"):
     from app.udaconnect import register_routes as attach_udaconnect
 
     # Add routes
-    attach_udaconnect(api, app)
+    attach_udaconnect(persons, app)
